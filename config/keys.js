962458bd3d68ed339f4a220a3f54e47278951a5c
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2018-07-09 22:50:20 -0300

    Add authentication with JWT

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  mongoURI: process.env.DB_URI
+  mongoURI: process.env.DB_URI,
+  secret: 'secret'
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const keys = require('../../config/keys');
 const User  = require('../../models/User');
+
 
 
 // @route   GET api/users/test
@@ -46,6 +51,45 @@ router.post('/register', async (req, res) => {
         });
       }
     });
+});
+
+// @route   GET api/users/login
+// @desc    Login user / Returnin JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email})
+    .then(user => {
+      if(!user) {
+        return res.status(404).json({email: 'User not found'});
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch) {
+
+            const payload = { id: user.id, name: user.name, avatar: user.avatar }
+            
+            jwt.sign(payload, keys.secret, { expiresIn: 3600 }, (err, token) => {
+              res.json({
+                success: true,
+                token: 'Bearer ' + token
+              })
+            });
+          } else {
+            return res.status(400).json({password: 'Password incorrect' });
+          }
+        })
+    });
+});
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current', passport.authenticate('jwt', {session: false }), (req, res) => {
+  res.json(req.user);
 });
 
 module.exports = router;
