f4100d2df6bec0e32d51238c3bd2ca09013237d8
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2018-08-11 11:26:21 -0300

    Add edit profile form

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
-
+import EditProfile from './components/edit-profile/EditProfile';
 
 import './App.css';
 
@@ -54,7 +54,9 @@ class App extends Component {
               <Switch>
                 <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
               </Switch>
-              
+              <Switch>
+                <PrivateRoute exact path="/edit-profile" component={ EditProfile } />
+              </Switch>
             </div>
             <Footer />
           </div>
