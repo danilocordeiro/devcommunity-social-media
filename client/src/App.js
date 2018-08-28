bac05557830d6039b386a847508e2366321fad86
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2018-08-27 21:26:19 -0300

    Add post and post form componente

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,9 @@ import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import NotFound from './components/not-found/NotFound';
-
+import Posts from './components/posts/Posts';
 import './App.css';
+
 
 if(localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
@@ -69,6 +70,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/add-education" component={ AddEducation } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={ Posts } />
               </Switch>
               <Route exact path="/not-found" component={ NotFound } />
             </div>
